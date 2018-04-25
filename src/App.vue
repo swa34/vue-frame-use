282029e42d739db7e1be53a947755464f206bb1d
Gabriel Benson <me@gabe.xyz>
2018-04-25 16:11:30 -0400

    started gacounts report

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 	import { getComputed, getStore } from '@/modules/store';
 	import { stringFormats, url } from '@/modules/utilities';
 	import DetailMain from '@/views/data/DetailMain';
-	import schema from '@/schemas/extension_website_content/educational_opportunity';
+	import schema from '@/schemas/gacounts3/report';
 
 	// Hacky fix for schemas without titles
 	if (!schema.title) schema.title = stringFormats.tableToTitle(schema.table);
--- src/schemas/gacounts3/report.js
+++ src/schemas/gacounts3/report.js
@@ -1,3 +1,22 @@
+// Gotta fetch activity locations
+let activityLocations = [
+	{
+		ID: 1,
+		LABEL: 'Inside Georgia',
+		USES_ALTERNATE_TEXT: 0
+	},
+	{
+		ID: 2,
+		LABEL: 'Outside Georgia, Inside U.S.',
+		USES_ALTERNATE_TEXT: 1
+	},
+	{
+		ID: 3,
+		LABEL: 'Outside U.S.',
+		USES_ALTERNATE_TEXT: 1
+	}
+];
+
 const schema = {
 	database: 'GACOUNTS3',
 	table: 'REPORT',
@@ -12,12 +31,23 @@ const schema = {
 			columnName: 'OWNER_ID',
 			type: 'int',
 			required: true,
-			immutable: true
+			constraint: {
+				database: 'CAES_CENTRAL_DATABASE',
+				table: 'PERSONNEL',
+				foreignKey: 'ID',
+				values: []
+			}
 		},
 		{
 			columnName: 'ACTUAL_SUBMITTER_ID',
 			type: 'int',
-			required: true
+			required: true,
+			constraint: {
+				database: 'CAES_CENTRAL_DATABASE',
+				table: 'PERSONNEL',
+				foreignKey: 'ID',
+				values: []
+			}
 		},
 		{
 			columnName: 'TITLE',
@@ -37,20 +67,48 @@ const schema = {
 		{
 			columnName: 'COUNTY_ID',
 			type: 'int',
-			required: true
+			required: true,
+			constraint: {
+				database: 'Portal',
+				table: 'CountyList',
+				foreignKey: 'CountyListID',
+				values: []
+			}
 		},
 		{
 			columnName: 'SCOPE_ID',
 			type: 'int',
-			required: true
+			required: true,
+			constraint: {
+				database: 'CAES_CENTRAL_DATABASE',
+				table: 'PROGRAM_SCOPE',
+				foreignKey: 'ID',
+				foreignLabel: 'LABEL',
+				values: []
+			}
 		},
 		{
 			columnName: 'ACTIVITY_LOCATION_TYPE_ID',
-			type: 'int'
+			type: 'int',
+			constraint: {
+				database: 'GACOUNTS3',
+				table: 'ACTIVITY_LOCATION_TYPE',
+				foreignKey: 'ID',
+				foreignLabel: 'LABEL',
+				values: []
+			}
 		},
 		{
 			columnName: 'ACTIVITY_LOCATION_ALTERNATE_TEXT',
-			type: 'nvarchar'
+			type: 'nvarchar',
+			depends: {
+				column: 'ACTIVITY_LOCATION_TYPE_ID',
+				test (val) {
+					const activityLocationMap = activityLocations.map(location => location.ID);
+					const activityLocationIndex = activityLocationMap.indexOf(val);
+					return activityLocationIndex !== -1 && activityLocations[activityLocationIndex].USES_ALTERNATE_TEXT;
+				}
+			}
 		},
 		{
 			columnName: 'DATE_CREATED',
