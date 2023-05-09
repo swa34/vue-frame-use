4eb59735c9289b0abccd897666addffe82bc95c8
scott_allen <scott.allen@uga.edu>
2023-05-09 06:18:27 -0400

    small changes

--- src/views/custom/caes_research_farm_project/CaesPersonnelDetails.vue
+++ src/views/custom/caes_research_farm_project/CaesPersonnelDetails.vue
@@ -43,13 +43,29 @@ export default {
 			isCoInvestigator: this.options.isCoInvestigator || false
 		};
 	},
+	//Removed options i.e this.options.isCoInvestigator dont know if it matters
 	computed: {
 		personnelId() {
-			if (this.isPrinciple) return this.$store.state.project.PI_PERSONNEL_ID;
+			console.log("isPrinciple: " + this.isPrinciple);
+			console.log("isCoInvestigator: " + this.isCoInvestigator);
+			if (this.isPrinciple) {
+				console.log(
+					"isPrinciple: " + this.$store.state.project.PI_PERSONNEL_ID
+				);
+				return this.$store.state.project.PI_PERSONNEL_ID;
+			}
 			// Added by SA 05/05/23
-			if (this.options.isCoInvestigator)
+			if (this.isCoInvestigator) {
+				console.log(
+					"isCoInvestigator: " + this.$store.state.project.CI_PERSONNEL_ID
+				);
 				return this.$store.state.project.CI_PERSONNEL_ID;
+			}
 			// End of addition by SA 05/05/23
+			console.log(
+				"SECONDARY_CONTACT_PERSONNEL_ID: " +
+					this.$store.state.project.SECONDARY_CONTACT_PERSONNEL_ID
+			);
 			return this.$store.state.project.SECONDARY_CONTACT_PERSONNEL_ID;
 		},
 		personnel() {
@@ -60,7 +76,7 @@ export default {
 			let listKey;
 			if (this.isPrinciple) {
 				listKey = "principleInvestigators";
-			} else if (this.options.isCoInvestigator) {
+			} else if (this.isCoInvestigator) {
 				listKey = "coInvestigators";
 			} else {
 				listKey = "secondaryContacts";
